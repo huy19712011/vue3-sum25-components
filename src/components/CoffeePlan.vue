aa6b2c8f3b5830b452b3c9f8279c79c023527da1
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-07-22 20:31:52 +0700

    V2: Reusable Components with Props

---
+++ src/components/CoffeePlan.vue
@@ -0,0 +1,15 @@
+<script setup>
+// defineProps(["name"]);
+defineProps({
+  name: { type: String, default: "Default Plan" },
+  //   price: { type: Number, required: false },
+});
+</script>
+
+<template>
+  <div class="plan">
+    <div class="description">
+      <span class="title"> {{ name }} </span>
+    </div>
+  </div>
+</template>
