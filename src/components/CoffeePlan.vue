51d59b7c6f5c98387d95899437cfb653fe4e14ba
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-07-23 20:06:48 +0700

    V5: Custom Events for Communication between Components

--- src/components/CoffeePlan.vue
+++ src/components/CoffeePlan.vue
@@ -1,13 +1,25 @@
 <script setup>
+import { ref } from "vue";
+
 // defineProps(["name"]);
-defineProps({
+const props = defineProps({
   name: { type: String, default: "Default Plan" },
   //   price: { type: Number, required: false },
+  selected: { type: Boolean, default: false },
 });
+
+// const selected = ref(false);
+
+const emit = defineEmits(["selected"]);
+
+const selectPlan = () => {
+  // selected.value = !selected.value;
+  emit("selected", props.name);
+};
 </script>
 
 <template>
-  <div class="plan">
+  <div class="plan" :class="{ 'active-plan': selected }" @click="selectPlan">
     <div class="description">
       <span class="title"> {{ name }} </span>
     </div>
