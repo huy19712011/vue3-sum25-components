933e82af92cfe6dc7b243a98a7a5703beb0336a9
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-07-23 17:58:01 +0700

    V3: Nested Components

---
+++ src/components/PlanPicker.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from "vue";
+import CoffeePlan from "./CoffeePlan.vue";
+
+// defineProps({
+//   plans: Array,
+// });
+
+const plans = ref(["The Single", "The Curious", "The Addict", "The Hacker"]);
+</script>
+<template>
+  <div class="plans">
+    <CoffeePlan v-for="plan in plans" :key="plan" :name="plan"></CoffeePlan>
+  </div>
+</template>
